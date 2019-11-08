5dfd8bc19c912179f4a3e39d8ed1882073a3fdea
skrudra <skrudra12@gmail.com>
2019-11-08 21:43:18 +0530

    refactor

--- src/main/java/com/example/springcloudstream/SpringcloudstreamApplication.java
+++ src/main/java/com/example/springcloudstream/SpringcloudstreamApplication.java
@@ -3,10 +3,7 @@ package com.example.springcloudstream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
-
-import com.example.springcloudstream.beans.Book;
 
 @SpringBootApplication
 @EnableBinding(Sink.class)
@@ -16,8 +13,4 @@ public class SpringcloudstreamApplication {
 		SpringApplication.run(SpringcloudstreamApplication.class, args);
 	}
 
-//	@StreamListener(Sink.INPUT)
-//	public void handle(Book book) {
-//		System.out.println("Received: " + book);
-//	}
 }
